4ca3562a622439219e568228d0e7fbd2f370df32
Maliamungu53 <90456910+Maliamungu53@users.noreply.github.com>
2021-09-12 19:29:21 +0300

    Update message.php

--- message.php
+++ message.php
@@ -1,6 +1,6 @@
 <?php
 
-	//let's get all form values 
+	
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$phone = $_POST['phone'];
@@ -10,10 +10,10 @@
 		if(filter_var($email, FILTER_VALIDATE_EMAIL)){
 			$reciever = "maliamunguemma72@gmail.com";
 			$subject = "From: $name <$email>";
-			// marging concatinating all user values inside body variables. \n is used for a new line
+			
 		$body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\nMessage: $message\n\nRegards,\n$name";
-		$sender = "From: $email"; //sender email 
-		if(mail($reciever, $subject, $body, $sender)){ //mail() is an inbuilt function to send mail
+		$sender = "From: $email";  
+		if(mail($reciever, $subject, $body, $sender)){ 
 			echo "Your message has been sent";
 		}else{
 			echo "Sorry! failed to send your message";
